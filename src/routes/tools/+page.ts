1b821ccab7e418bb2bea3eba2205532eefd16c1a
hoah2333 <z13725385251@163.com>
2024-04-08 21:48:18 +0800

    修复在github上无法使用server的问题

--- src/routes/tools/+page.ts
+++ src/routes/tools/+page.ts
@@ -1,6 +1,15 @@
+import type { pagesJsonType } from '$lib/types';
 import type { PageLoad } from './$types';
 
-export const load: PageLoad = async ({ parent, data }) => {
-	await parent();
-	return data ;
+export const load: PageLoad = async ({ fetch }) => {
+	let pagesJson: pagesJsonType;
+	const response = await fetch('https://cn-cd-dx-tmp17.natfrp.cloud:32470/', {
+		method: 'GET'
+	});
+
+	if (!response.ok) {
+		throw new Error(response.status + response.statusText);
+	}
+	pagesJson = await response.json();
+	return pagesJson;
 };
