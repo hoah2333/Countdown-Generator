c67260744936b1c85f575dfc710292ee030dbe87
hoah2333 <z13725385251@163.com>
2024-04-09 01:04:57 +0800

    优化错误报告

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -3,8 +3,9 @@
 </script>
 
 <h3>
-	糟糕！这个页面出现了 {$page.status}
-	{$page.error ? $page.error.message : ''}！如果这不是有意为之的，请向工具作者
+	糟糕！这个页面出现了 <a href="https://developer.mozilla.org/zh-CN/docs/Web/HTTP/Status/{$page.status}">{$page.status}</a> 错误！原因为：{$page.error ? $page.error.message : ''}！
+	<br />
+	如果这不是有意为之的，请向工具作者
 	<a href="http://www.wikidot.com/account/messages#/new/4915183">hoah2333</a> 报告！
 </h3>
 
--- src/routes/tools/+page.ts
+++ src/routes/tools/+page.ts
@@ -1,14 +1,28 @@
 import type { pagesJsonType } from '$lib/types';
+import { error } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 export const load: PageLoad = async ({ fetch }) => {
 	let pagesJson: pagesJsonType;
+
+	try {
+		await fetch('https://cn-cd-dx-tmp17.natfrp.cloud:32470/', {
+			method: 'GET',
+            credentials: 'omit'
+		});
+	} catch (e) {
+		error(503, `Br_Bot 服务器未响应，原因：${e}`)
+	}
+
 	const response = await fetch('https://cn-cd-dx-tmp17.natfrp.cloud:32470/', {
-		method: 'GET'
+        method: 'GET',
+        credentials: 'omit'
 	});
 
 	if (!response.ok) {
-		throw new Error(response.status + response.statusText);
+		error(response.status, `Br_Bot 服务器未响应，原因：${response.statusText}`)
 	}
 	pagesJson = await response.json();
 	return pagesJson;
