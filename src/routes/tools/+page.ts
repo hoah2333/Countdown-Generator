47c9254351bb0bd364d2c3dc78eafc3d14ddb2db
hoah2333 <z13725385251@163.com>
2024-04-10 01:34:07 +0800

    优化fetch流程

--- src/routes/tools/+page.ts
+++ src/routes/tools/+page.ts
@@ -7,19 +7,17 @@ import type { PageLoad } from './$types';
 export const load: PageLoad = async ({ fetch }) => {
 	let pagesJson: pagesJsonType;
 
+	let response: Response;
+
 	try {
-		await fetch('https://cn-cd-dx-tmp17.natfrp.cloud:32470/', {
+		response = await fetch('https://cn-cd-dx-tmp17.natfrp.cloud:32470/', {
 			method: 'GET',
 			credentials: 'omit'
 		});
 	} catch (e) {
+		console.error(e);
 		error(503, `Br_Bot 服务器未响应，原因：${e}`);
 	}
-
-	const response = await fetch('https://cn-cd-dx-tmp17.natfrp.cloud:32470/', {
-		method: 'GET',
-		credentials: 'omit'
-	});
 
 	if (!response.ok) {
 		error(response.status, `Br_Bot 服务器未响应，原因：${response.statusText}`);
