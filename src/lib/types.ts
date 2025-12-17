209c63f3bfe41390652ffc3bac2c4e8b62a561ca
hoah2333 <c3n6fak@gmail.com>
2025-12-17 22:42:40 +0800

    prettier

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,43 +1,35 @@
 export interface pagesJsonType {
-    pre_delete_pages: [
-        {
-            link: string;
-            title: string;
-            score: number;
-            release_score: number;
-            time: number;
-            discuss_link: string;
-            post_id: number;
-            isOriginal: boolean;
-            timestamp: number;
-        }
-    ];
-    deleted_pages: [
-        {
-            link: string;
-            title: string;
-            score: number;
-            time: number;
-            context: string;
-            page_type: string[];
-            release_score: number;
-        }
-    ];
-    errors: [
-        {
-            threadId: number;
-            postId: number;
-            title: string;
-            source: string;
-            errorType: string;
-        }
-    ];
-    update_timestamp: number;
+  pre_delete_pages: [
+    {
+      link: string;
+      title: string;
+      score: number;
+      release_score: number;
+      time: number;
+      discuss_link: string;
+      post_id: number;
+      isOriginal: boolean;
+      timestamp: number;
+    },
+  ];
+  deleted_pages: [
+    {
+      link: string;
+      title: string;
+      score: number;
+      time: number;
+      context: string;
+      page_type: string[];
+      release_score: number;
+    },
+  ];
+  errors: [{ threadId: number; postId: number; title: string; source: string; errorType: string }];
+  update_timestamp: number;
 }
 
 export interface timerTextType {
-    warning: string;
-    text: string;
-    timer: string;
-    status: string;
+  warning: string;
+  text: string;
+  timer: string;
+  status: string;
 }
--- src/routes/timer/[...params]/+page.svelte
+++ src/routes/timer/[...params]/+page.svelte
@@ -1,33 +1,28 @@
 <script lang="ts">
-    import { page } from "$app/stores";
-    import { timerFunc } from "$lib/timer";
-    import type { timerTextType } from "$lib/types";
-    import { onDestroy } from "svelte";
+  import { page } from "$app/stores";
+  import { timerFunc } from "$lib/timer";
+  import type { timerTextType } from "$lib/types";
+  import { onDestroy } from "svelte";
 
-    let timerTexts: timerTextType = {
-        warning: "",
-        status: "",
-        text: "",
-        timer: ""
-    };
+  let timerTexts: timerTextType = { warning: "", status: "", text: "", timer: "" };
 
-    const interval = setInterval(() => {
-        timerTexts = timerFunc($page.params.params.split("/"));
-    }, 1000);
+  const interval = setInterval(() => {
+    timerTexts = timerFunc($page.params.params.split("/"));
+  }, 1000);
 
-    onDestroy(() => {
-        clearInterval(interval);
-    });
+  onDestroy(() => {
+    clearInterval(interval);
+  });
 </script>
 
 <div id="countdown-wrapper">
-    {#if typeof timerTexts === "object"}
-        <span class="warning">{timerTexts.warning}</span>
-        <span class="text {timerTexts.status}">{timerTexts.text}</span><br />
-        <span class="timer">{timerTexts.timer}</span>
-    {/if}
+  {#if typeof timerTexts === "object"}
+    <span class="warning">{timerTexts.warning}</span>
+    <span class="text {timerTexts.status}">{timerTexts.text}</span><br />
+    <span class="timer">{timerTexts.timer}</span>
+  {/if}
 </div>
 
 <style lang="scss" type="text/scss">
-    @import "./page";
+  @import "./page";
 </style>
